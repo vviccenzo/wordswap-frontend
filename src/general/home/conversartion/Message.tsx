137915a4eaa83fe830893a24f0c8df146f1a095a
vviccenzo <vvpcraft@gmail.com>
2024-08-22 06:58:44 -0300

    SCRUM-11 feat: - Implementação inicial de lógica para gerar menu na mensagem clicando com o botão direito

--- src/general/home/conversartion/Message.tsx
+++ src/general/home/conversartion/Message.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
-import { List, Typography } from 'antd';
+import { List, Typography, Menu, Dropdown } from 'antd';
 
 const { Text } = Typography;
 
 export function Message({ message, isMe }) {
+
+
+    const menu = (
+        <Menu>
+            <Menu.Item key="edit" onClick={() => handleMenuClick('edit')}>
+                Editar mensagem
+            </Menu.Item>
+            <Menu.Item key="delete" onClick={() => handleMenuClick('delete')}>
+                Apagar mensagem
+            </Menu.Item>
+        </Menu>
+    );
+
+    const handleMenuClick = (key) => {
+        if (key === 'edit') {
+            console.log(key);
+        } else if (key === 'delete') {
+            console.log(key);
+        }
+    };
+
     return (
         <List.Item
             style={{
@@ -13,27 +34,33 @@ export function Message({ message, isMe }) {
                 marginRight: isMe ? 10 : '0',
             }}
         >
-            <div
-                style={{
-                    background: isMe ? '#dcf8c6' : '#fff',
-                    borderRadius: '8px',
-                    padding: '8px 16px',
-                    maxWidth: '60%',
-                    wordBreak: 'break-word',
-                }}
+            <Dropdown
+                overlay={menu}
+                trigger={['contextMenu']}
             >
-                <div className="message-content" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end' }}>
-                    <Text style={{ marginRight: '10px' }}>{message.content}</Text>
-                    <span style={{
-                        fontSize: '10px',
-                        color: '#888',
-                        flexShrink: 0,
-                        marginBottom: '1px',
-                    }}>
-                        {message.timestamp}
-                    </span>
+                <div
+                    style={{
+                        background: isMe ? '#dcf8c6' : '#fff',
+                        borderRadius: '8px',
+                        padding: '8px 16px',
+                        maxWidth: '60%',
+                        wordBreak: 'break-word',
+                    }}
+                >
+                    <div className="message-content" style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end' }}>
+                        <Text style={{ marginRight: '10px' }}>{message.content}</Text>
+                        <span style={{
+                            fontSize: '10px',
+                            color: '#888',
+                            flexShrink: 0,
+                            marginBottom: '1px',
+                        }}>
+                            {message.timestamp}
+                        </span>
+                    </div>
                 </div>
-            </div>
+            </Dropdown>
         </List.Item>
+
     );
 }
