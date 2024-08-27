2c908d733bb2c15d97a6a2bad22e913681dec597
vviccenzo <vvpcraft@gmail.com>
2024-08-26 22:19:18 -0300

    SCRUM-19 feat: - Adicionando lógica inicial para efetuar request de paginação

--- src/general/home/conversartion/chat/ChatBody.tsx
+++ src/general/home/conversartion/chat/ChatBody.tsx
@@ -1,18 +1,8 @@
 import React from 'react';
 import { List } from 'antd';
+import VirtualList from 'rc-virtual-list';
 import { Message } from '../Message.tsx';
 import formatTimestamp from '../../../../utils/formatTimestamp.ts';
-
-interface MessageProps {
-    id: string;
-    content: string;
-    avatar?: string;
-    senderName: string;
-    timestamp: string;
-    isEdited: boolean;
-    isDeleted: boolean;
-    messageContent: any;
-}
 
 interface ChatBodyProps {
     messages: any[];
@@ -20,28 +10,40 @@ interface ChatBodyProps {
 }
 
 export function ChatBody({ messages, selectedConversation }: ChatBodyProps) {
+    const onScroll = (e: React.UIEvent<HTMLElement, UIEvent>) => {
+        if (e.currentTarget.scrollTop === 0) {
+            console.log('loadaaaaa')
+        }
+    };
+
     return (
         <div className="chat-container">
-            <List
-                itemLayout="horizontal"
-                dataSource={messages}
-                renderItem={(msg) => (
-                    <Message
-                        message={{
-                            id: msg.id,
-                            content: msg.content,
-                            avatar: msg.sender === 'me' ? null : selectedConversation?.profilePicture,
-                            senderName: msg.sender === 'me' ? 'You' : selectedConversation?.conversationName,
-                            timestamp: formatTimestamp(msg.timeStamp),
-                            isEdited: msg.isEdited,
-                            isDeleted: msg.isDeleted,
-                            messageContent: msg.messageContent
-                        }}
-                        isMe={msg.sender === 'me'}
-                        conv={selectedConversation}
-                    />
-                )}
-            />
+            <List itemLayout="horizontal">
+                <VirtualList
+                    data={messages}
+                    height={1000}
+                    itemHeight={47}
+                    itemKey="id"
+                    onScroll={onScroll}
+                >
+                    {(msg) => (
+                        <Message
+                            message={{
+                                id: msg.id,
+                                content: msg.content,
+                                avatar: msg.sender === 'me' ? null : selectedConversation?.profilePicture,
+                                senderName: msg.sender === 'me' ? 'You' : selectedConversation?.conversationName,
+                                timestamp: formatTimestamp(msg.timeStamp),
+                                isEdited: msg.isEdited,
+                                isDeleted: msg.isDeleted,
+                                messageContent: msg.messageContent
+                            }}
+                            isMe={msg.sender === 'me'}
+                            conv={selectedConversation}
+                        />
+                    )}
+                </VirtualList>
+            </List>
         </div>
     );
 }
